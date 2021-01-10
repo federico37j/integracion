8eae4fc14ec45d3e87738f1f7a242bcb4f0cbebc
Federico <federicoarambillet37@gmail.com>
2021-01-09 23:03:03 -0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { ProductoModule } from './producto/producto.module';
 import { ClienteModule } from './cliente/cliente.module';
 import { FacturaModule } from './factura/factura.module';
 import { TelefonoModule } from './telefono/telefono.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';  
 
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      }),
     TypeOrmModule.forRoot()
     , ProductoModule, ClienteModule, FacturaModule, TelefonoModule],
   controllers: [AppController],
